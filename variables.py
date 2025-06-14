4a7f680ff073f2e1b7fa8bcc83f92b107e0c4b45
Alejandro Coman Venceslá <alejandrocomanvencesla@gmail.com>
2025-06-14 14:53:51 +0200

    Subida del TFG a un respositorio

---
+++ variables.py
@@ -0,0 +1,20 @@
+API_KEY = "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"
+
+modelos = [
+    "openai/gpt-4o-mini",
+    "deepseek/deepseek-chat-v3-0324",
+    "google/gemini-2.0-flash-001",
+    "microsoft/phi-4-multimodal-instruct",
+    "meta-llama/llama-4-maverick"
+]
+
+siglos = ["20th", "21st"]
+
+paises = ["Germany", "Argentina", "Brasil", "Canada", "China",
+            "South Korea", "United States", "France", "Italy", 
+           "Japan", "Mexico", "Netherlands", "Norway", "Poland", 
+           "Portugal", "United Kingdom", "Russia", "Turkey", "Spain"]
+
+ambito = ["economists", "politicians", "scientists", "writers", "athletes", "artists", "philosophers", "musicians", "inventors"]
+
+genero = ["male", "female"]
